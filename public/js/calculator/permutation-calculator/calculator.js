65413070f7fc0b73de3b6193f5697d1d18c64d0e
GodHad <sunharius@gmail.com>
2024-08-09 22:30:47 -0700

    remove unnecessary files

--- public/js/calculator/permutation-calculator/calculator.js
+++
@@ -1,9 +0,0 @@
-function calculate() {
-	let n = input.get('n').gte(0).integer().val();
-	let r = input.get('r').lte('n').integer().val();
-	if(!input.valid()) return;
-	const result = math.permutations(math.bignumber(n),math.bignumber(r));
-	_('result').innerHTML = result.toFixed().length > 500 ? result : result.toFixed();
-}
-
-window.addEventListener('load', () => math.config({number: 'BigNumber', precision: 500}));
